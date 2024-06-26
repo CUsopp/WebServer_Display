e2a4c75492ec94862bc6c1ba9e6e8752674cc723
CUsopp <228683942@qq.com>
2024-06-26 16:06:27 +0800

    tinyWeb_threadPool_interface

--- TinyWebServer/http/http_conn.cpp
+++ TinyWebServer/http/http_conn.cpp
@@ -1,5 +1,34 @@
 #include "http_conn.h"
 
+//初始化新接受的连接
+//check_state默认为分析请求行状态
+void http_conn::init()
+{
+    //mysql = NULL;
+    //bytes_to_send = 0;
+    //bytes_have_send = 0;
+    //m_check_state = CHECK_STATE_REQUESTLINE;
+    //m_linger = false;
+    //m_method = GET;
+    //m_url = 0;
+    //m_version = 0;
+    //m_content_length = 0;
+    //m_host = 0;
+    //m_start_line = 0;
+    //m_checked_idx = 0;
+    m_read_idx = 0;
+    //m_write_idx = 0;
+    //cgi = 0;
+    //m_state = 0;
+    //timer_flag = 0;
+    improv = 0;
+
+    memset(m_read_buf, '\0', READ_BUFFER_SIZE);
+    //memset(m_write_buf, '\0', WRITE_BUFFER_SIZE);
+    //memset(m_real_file, '\0', FILENAME_LEN);
+}
+
+//sqlpool
 void http_conn::initmysql_result(connection_pool *connPool)
 {
 	//先从连接池中取一个连接
@@ -30,3 +59,49 @@ void http_conn::initmysql_result(connection_pool *connPool)
 	}
 	return;
 }
+
+//threadpooli
+//循环读取客户数据，直到无数据可读或对方关闭连接
+//边缘触发和水平触发两种方式读数据
+bool http_conn::read_once()
+{
+	if(m_read_idx >= READ_BUFFER_SIZE)
+	{
+		return false;
+	}
+	int bytes_read = 0;
+	
+	//LT读取数据 水平触发
+	if(0 == m_TRIGMode)
+	{
+		bytes_read = recv(m_sockfd, m_read_buf + m_read_idx, READ_BUFFER_SIZE - m_read_idx, 0);
+		m_read_idx += bytes_read;
+
+		if(bytes_read <= 0)
+		{
+			return false;
+		}
+		return true;
+	}
+	else//边缘触发
+	{
+		while(true)
+		{
+			bytes_read = recv(m_sockfd, m_read_buf + m_read_idx, READ_BUFFER_SIZE - m_read_idx, 0);
+			if(bytes_read == -1)
+			{
+				if(errno == EAGAIN || errno == EWOULDBLOCK)
+					break;
+				return false;
+			}
+			else if(bytes_read == 0)
+			{
+				return false;
+			}
+			m_read_idx += bytes_read;
+		}
+		return true;
+	}
+}
+
+
--- TinyWebServer/http/http_conn.h
+++ TinyWebServer/http/http_conn.h
@@ -3,19 +3,79 @@
 
 #include "../CGImysql/sql_connection_pool.h"
 #include <map>
+#include <sys/socket.h>
 
 class http_conn
 {
+public:
+    static const int FILENAME_LEN = 200;
+    static const int READ_BUFFER_SIZE = 2048;
+    static const int WRITE_BUFFER_SIZE = 1024;
+    enum METHOD
+    {
+        GET = 0,
+        POST,
+        HEAD,
+        PUT,
+        DELETE,
+        TRACE,
+        OPTIONS,
+        CONNECT,
+        PATH
+    };
+    enum CHECK_STATE
+    {
+        CHECK_STATE_REQUESTLINE = 0,
+        CHECK_STATE_HEADER,
+        CHECK_STATE_CONTENT
+    };
+    enum HTTP_CODE
+    {
+        NO_REQUEST,
+        GET_REQUEST,
+        BAD_REQUEST,
+        NO_RESOURCE,
+        FORBIDDEN_REQUEST,
+        FILE_REQUEST,
+        INTERNAL_ERROR,
+        CLOSED_CONNECTION
+    };
+    enum LINE_STATUS
+    {
+        LINE_OK = 0,
+        LINE_BAD,
+        LINE_OPEN
+    };
+
 public:
 	http_conn(){}
 	~http_conn(){}
 
 public:
+	//sqlconnpool
 	void initmysql_result(connection_pool *connPool);
+
+	//threadpool
+	//void init(int sockfd, const sockaddr_in &addr, char *, int, int, string user, string passwd, string sqlname);
+	bool read_once();
+	bool write();
+	void process();
+	int timer_flag;
+	int improv;
+
+private:
+	void init();
 
 private:
 	map<string, string> m_users;
 	int m_close_log;
+	int m_TRIGMode;//1-ET-边缘触发 0-LT-水平触发
+
+private:
+	int m_sockfd;
+	char m_read_buf[READ_BUFFER_SIZE];
+	long m_read_idx;
+	
 };
 
 
